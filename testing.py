1d5ef95511b90bd13167fee01e84486a9d8f401f
rajisenthilkumar2012-hub <raji.senthilkumar2012@gmail.com>
2025-08-06 21:29:51 -0700

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,25 @@
+print("hi, welcome to the story of choices you will hav eto pick between 2 choices and one will kill you and the other will let you continue, so lets begin")
+print("You first decision is to decide if you will go through teh slightly shorter path of going to teh dangerous forst, or going through slightly longer path throught the village")
+print("a is the first option, the forest,and b is the second option teh safer village")
+x=input("choose option a or b :")
+if x== "a":
+    print("you picked the wrong option, this was supposed to be easy, byeee" )
+else:
+    print("You picked the right option, for now")
+print("now its time for you next decision")
+y=input("the options are, either go and save the person screaming in the forest or go in the forest but the opposite direction, pick a if you save or pick b if you walk away")
+if y=="a":
+    print("you picked the right option good job bro")
+else:
+    print("How dare you pick the wrong option ")
+print(" you have saved the person, very good job very young tadpole I hope you aree well, and if you had to restart at any point, your descisions were very badddd")
+prnt(" the is a bonus descision if you want to do it, if you do not want to do it, just type no")
+print("now you have to make a choice, you can either go back to the village or go deeper into the forest")
+z=input("pick a to go back to the village or b to go deeper into the forest")
+if z=="a":
+    print("you have made the right choice, you are now safe in the village")
+else:
+    print("you have made the wrong choice, you are now lost in the forest, and you will never be found again, good job bro")    
+    print("the end, you have made it to the end of the story, if you want to play again, just run the code again")
+print("one last thing, if you liked  this game reccomend to you besties so we can all be besties")
+print("and if you hate this game,just keep it to yourself because people willl think your crazy so I ma just helping you, your welcome")
